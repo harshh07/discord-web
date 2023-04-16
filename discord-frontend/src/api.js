7e0376f9606a36f787d38b1c50325e8aa08202b8
Harsh <58371468+harshh07@users.noreply.github.com>
2023-04-16 13:46:46 +0530

    Updated api.js path to container name

--- discord-frontend/src/api.js
+++ discord-frontend/src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { logout } from "./shared/utils/auth";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:5002/api",
+  baseURL: "http://discord-backend:5002/api",
   //timeout: 1000,
 });
 
